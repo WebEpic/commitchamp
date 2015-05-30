df52f1acd937b90276d9a727bfc669e276acfafa
WebEpic <randy@randyjmitchell.com>
2015-05-30 18:08:25 -0400

    added OAuth and GET method for contributions

--- lib/commitchamp/github.rb
+++ lib/commitchamp/github.rb
@@ -1,8 +1,27 @@
 require 'httparty'
 
 module Commitchamp
+
+  OAUTH_TOKEN = ENV['OAUTH_TOKEN']
+
   class Github
     include HTTParty
     base_uri "https://api.github.com"
+
+    def initialize
+      @headers = { "Authorization" => "token #{OAUTH_TOKEN}",
+                   "User-Agent" => "HTTParty" }
+    end
+
+    def get_contributions(owner, repo, page=1)
+      params = {
+        page: page
+      }
+      options = {
+        headers: headers,
+        query: params
+      }
+      self.class.get("repos/#{owner}/#{repo}/stats/contributors", options)
+    end
   end
 end
